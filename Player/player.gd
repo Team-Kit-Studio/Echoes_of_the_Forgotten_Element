97549e9e0e2214cb31355481e0945d220e19cef8
Mundolor <maksimka0776@gmail.com>
2025-02-10 19:44:06 +0300

    Самый последний результат

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 enum {
 	MOVE,
 	DAMAGE,
@@ -8,70 +7,72 @@ enum {
 	DEATH
 }
 
-var alive = true
-var health = 100
-var speed = 200.0
-var state = MOVE
-var combo = false
-
 
 @onready var anim = $AnimatedSprite2D
-@onready var animPlayer = $AnimationPlayer
+@onready var AnimPlayer = $AnimationPlayer
+
+const speed = 100.0
+
+const direction_run: Dictionary = {
+	Vector2.RIGHT: "State_Run_FromSide",
+	Vector2.LEFT: "State_Run_FromSide",
+	Vector2.UP: "State_Run_Up",
+	Vector2.DOWN: "State_Run_Down" 
+}
+
+const direction_idle: Dictionary = {
+	Vector2.RIGHT: "State_Idle_FromSide",
+	Vector2.LEFT: "State_Idle_FromSide",
+	Vector2.UP: "State_Idle_Up",
+	Vector2.DOWN: "State_Idle_Down" 
+}
+
+var alive = true
+var health = 100
+var state = MOVE
+var combo = false
+var input_direction = Vector2.ZERO
+var last_direction: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	match state:
 		MOVE:
-			move_state()
+			Move_State()
 		DAMAGE:
 			pass
 		ATTACK:
-			attack_state()
+			pass
 		DEATH:
-			death_state()
+			pass
 	move_and_slide()
-
-func move_state():
-	if alive == true:
-		var input_direction = Input.get_vector("left","right","up","down").normalized()
-		if input_direction:
-			velocity = input_direction * speed
-			animPlayer.play("State_Run")
-			print(input_direction)
-			if input_direction == 1:
-				print("доп доп ес ес")
-		else:
-			velocity = input_direction * 0
-			animPlayer.play("State_Idle_fromsife")
-		if input_direction.x < 0:
-			anim.flip_h = true
-		elif input_direction.x > 0:
-			anim.flip_h = false
-		if Input.is_action_just_pressed('click'):
-			state = ATTACK
-		if health <= 0:
-			state = DEATH
+	
+	
+func Move_State():
+	input_direction = Input.get_vector("left", "right", "up", "down").normalized()
+	
+	if input_direction != Vector2.ZERO:
+		# Устанавливаем скорость передвижения
+		velocity = input_direction * speed
 		
-	
-	#queue_free()
-func attack_state():
-	#if Input.is_action_just_pressed("click") and combo == true:# комбо
-		#state = ATTACK
-	velocity.x =0
-	velocity.y =0
-	animPlayer.play("Shoot")
-	await animPlayer.animation_finished
-	state = MOVE
-func combo_shoot():
-	combo = true
-	await animPlayer.animation_finished
-	combo = false
-	
-func death_state():
-	alive = false
-	health = 0
-	velocity.x = 0
-	velocity.y = 0
-	animPlayer.play("Death")
-	await animPlayer.animation_finished
-	animPlayer.pause()
-	anim.visible = false
+		# Сохраняем последнее направление
+		last_direction = input_direction
+		
+		if last_direction.x != 0:  # То есть если есть горизонтальное движение
+			anim.flip_h = last_direction.x > 0
+		# Управляем анимацией бега
+		if direction_run.has(last_direction):
+			play_animation(direction_run[last_direction])
+	else:
+		velocity = Vector2.ZERO  # Останавливаем персонажа
+
+		# Играть анимацию бездействия, основываясь на последнем направлении
+		if last_direction != Vector2.ZERO and direction_idle.has(last_direction):
+			play_animation(direction_idle[last_direction])
+		else:
+			# Если не было движения, переключаем на обычную анимацию бездействия
+			play_animation("State_Idle_FromSide")
+		
+func play_animation(animation: String) -> void:
+
+	#print("Играет анимация: ", animation)
+	AnimPlayer.play(animation)
