038dab806324408b03e31a7d21a3d4b0a4951ccb
Cool_HoHov <160066717+summeris124@users.noreply.github.com>
2025-02-13 17:10:38 +0200

    save update 12.02.2025

--- systems/save_system/save_sustem.gd
+++ systems/save_system/save_sustem.gd
@@ -5,13 +5,13 @@ const save_path: String = "user://save/"
 
 static func save_game(save_name: String = "default_save") -> void:
 	var save_path: String = save_path + save_name + ".json"
-	var file_save: SaveDataDefault = SaveDataDefault.new(save_path, Time.get_datetime_dict_from_system())
+	var file_save: SaveDataDefault = SaveDataDefault.new(save_name + ".json", Time.get_datetime_dict_from_system())
 	var file: FileAccess = FileAccess.open(save_path, FileAccess.WRITE)
 	if file and file.get_open_error() == OK:
 		var count: int = 0
 		print("Сохранение успешно: ", save_path)
 		for node in Persistence.array_save:
-			add_object(file_save, node.name, node, true)
+			add_object(file_save, node.name, node, true, node.get_groups()[0])
 			file_save.data["level_data"]["list_objects"][node.name]["obj_data"]["pos_x"] = node.position.x
 			file_save.data["level_data"]["list_objects"][node.name]["obj_data"]["pos_y"] = node.position.y
 			count += 1
@@ -41,8 +41,8 @@ static func save_load(save_name: String, _key_loaded: String) -> Dictionary:
 		return {}
 	return {}
 
-static func add_object(file_save: SaveDataDefault, obj_name: String, node: Node, instance: bool) -> Dictionary:
-	var obj: DefaultDataObject = DefaultDataObject.new(obj_name, node, instance, "save_it")
+static func add_object(file_save: SaveDataDefault, obj_name: String, node: Node, instance: bool, group_name: String) -> Dictionary:
+	var obj: DefaultDataObject = DefaultDataObject.new(obj_name, node, instance, group_name)
 	file_save.data["level_data"]["list_objects"][obj_name] = obj.object
 	return obj.object
 	
