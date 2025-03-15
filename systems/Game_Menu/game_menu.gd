f4e6b8175c07067cd37b572ea3ca0d2e9b02df6f
Hoyv <retik253@gmail.com>
2025-03-16 01:10:30 +0200

    save progress

--- systems/Game_Menu/game_menu.gd
+++ systems/Game_Menu/game_menu.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var settings: TabContainer = $CanvasLayer/GUI/Settings
 @onready var pauseButton: Button = $CanvasLayer/Pause
 @onready var saveLoadButton: Button = $"CanvasLayer/Main_Menu/PanelContainer/HBoxContainer/VBoxContainer/SaveLoad_Game"
-@onready var options: Button = $CanvasLayer/Main_Menu/PanelContainer/HBoxContainer/VBoxContainer/Options
+@onready var optionsButton: Button = $CanvasLayer/Main_Menu/PanelContainer/HBoxContainer/VBoxContainer/Options
 @onready var saveMenu: Control = $CanvasLayer/GUI/SaveMenu
 @onready var colorrect: ColorRect = $CanvasLayer/Load_visible
 
@@ -15,13 +15,10 @@ func _ready() -> void:
 	settings.visible = false	
 	pauseButton.visible = true
 
-func _physics_process(_delta: float) -> void:
+func _unhandled_key_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		toggle()
 
-	if not saveMenu.visible:
-		saveLoadButton.button_pressed = false
-	
 func load_control_from_settings() -> void:
 	var keybinds: Dictionary[String, InputEvent] = SettingsLoader.load_control_settings()
 	for action: String in keybinds.keys():
@@ -30,10 +27,11 @@ func load_control_from_settings() -> void:
 	
 func toggle() -> void:
 	visible = !visible
-	pauseButton.visible = not visible
-	mainMenu.visible = not mainMenu.visible
+	pauseButton.visible = !pauseButton.visible	
+	mainMenu.visible = !mainMenu.visible
 	saveMenu.visible = false
 	settings.visible = false
+	off_toggeled()
 	
 func _on_options_toggled(toggled_on: bool) -> void:
 	saveMenu.hide()
@@ -44,7 +42,6 @@ func _on_options_toggled(toggled_on: bool) -> void:
 		settings.visible = true
 		tween.tween_property(settings, "modulate", Color(1, 1, 1, 1), 0.30) 
 		tween.tween_property(settings, "position:x", 310, 0.2)
-
 	else:
 		tween.tween_property(settings, "modulate", Color(1, 1, 1, 0.1), 0.15) 
 		tween.tween_property(settings, "position:x", -240, 0.2)
@@ -52,34 +49,42 @@ func _on_options_toggled(toggled_on: bool) -> void:
 		settings.visible = false
 	
 func _on_continue_pressed() -> void:
-	mainMenu.visible = false
-	saveMenu.visible = false
-	pauseButton.visible = true
-	settings.hide()
-	if not settings.visible:
-		options.button_pressed = false
+	continues()
 
 func _on_button_pressed() -> void:
-	mainMenu.visible = true
-	pauseButton.visible = false
+	toggle()
+	
 
-func _on_load_game_pressed() -> void:
-	pass
+func continues() -> void:
+	pauseButton.visible = true
+	mainMenu.visible = false
+	saveMenu.visible = false
+	off_toggeled()
+	settings.hide()
+
+	if not settings.visible:
+		optionsButton.button_pressed = false
+
+	if not saveMenu.visible:
+		saveLoadButton.button_pressed = false
+
+func off_toggeled() -> void:
+	optionsButton.button_pressed = false
+	saveLoadButton.button_pressed = false
 
 func _on_save_game_toggled(toggled_on: bool) -> void:
 	settings.hide()
 	var tween: Tween = get_tree().create_tween()
 	tween.set_parallel(true)
 	if toggled_on:
-		options.button_pressed = false
+		optionsButton.button_pressed = false
 		saveMenu.visible = true
 		tween.tween_property(saveMenu, "modulate", Color(1, 1, 1, 1), 0.30) 
 		tween.tween_property(saveMenu, "position:x", 310, 0.2)
-		
 	else:
 		tween.tween_property(saveMenu, "modulate", Color(1, 1, 1, 0.1), 0.15) 
 		tween.tween_property(saveMenu, "position:x", -240, 0.2)
-		await tween.finished
+		await(tween.finished)
 		saveMenu.visible = false
 
 
@@ -91,9 +96,11 @@ func _on_exit_menu_pressed() -> void:
 	get_tree().call_deferred("change_scene_to_file", "res://systems/Main_Menu/Main_Menu.tscn")
 
 func hide_canvas() -> void:
+	mainMenu.hide()
 	canvas.hide()
 
 func show_canvas() -> void:
+	mainMenu.show()
 	canvas.show()
 
 func color_rect_show() -> void:
