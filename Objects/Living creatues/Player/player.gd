65298314901524e4dc14f10009de17cbfbfaad20
Hoyv <retik253@gmail.com>
2025-03-16 02:16:06 +0200

    StableVersions-Update

--- Objects/Living creatues/Player/player.gd
+++ Objects/Living creatues/Player/player.gd
@@ -13,18 +13,22 @@ enum state {
 
 const speed: int = 100
 
-const direction_run: Dictionary[Vector2, String]  = {
-	Vector2.RIGHT: "State_Run_FromSide",
-	Vector2.LEFT: "State_Run_FromSide",
-	Vector2.UP: "State_Run_Up",
-	Vector2.DOWN: "State_Run_Down" 
-}
+const anim_list: Dictionary[StringName, Dictionary] = {
+	StringName("idle"): {
+		Vector2i.RIGHT: "State_Idle_FromSide",
+		Vector2i.LEFT: "State_Idle_FromSide",
+		Vector2i.UP: "State_Idle_Up",
+		Vector2i.DOWN: "State_Idle_Down",
+		Vector2i.ZERO: "State_Idle_FromSide"
+	},
 
-const direction_idle: Dictionary[Vector2, String] = {
-	Vector2.RIGHT: "State_Idle_FromSide",
-	Vector2.LEFT: "State_Idle_FromSide",
-	Vector2.UP: "State_Idle_Up",
-	Vector2.DOWN: "State_Idle_Down" 
+	StringName("run"): {
+		Vector2i.RIGHT: "State_Run_FromSide",
+		Vector2i.LEFT: "State_Run_FromSide",
+		Vector2i.UP: "State_Run_Up",
+		Vector2i.DOWN: "State_Run_Down",
+		Vector2i.ZERO: ""
+	}
 }
 
 var alive: bool = true
@@ -32,47 +36,43 @@ var health: int = 100
 var current_state: state = state.MOVE
 var combo: bool = false
 var input_direction: Vector2 = Vector2.ZERO
-var last_direction: Vector2 = Vector2.ZERO
+var last_direction: Vector2i = Vector2.ZERO
 
 
 func _physics_process(_delta: float) -> void:
 	match current_state:
 		state.MOVE:
 			Move_State()
+
 		state.DAMAGE:
 			pass
+
 		state.ATTACK:
 			pass
+
 		state.DEATH:
 			pass
-	move_and_slide()
-	
 	
 func Move_State() -> void:
 	input_direction = Input.get_vector("left", "right", "up", "down")
 	if input_direction != Vector2.ZERO:
 
+		last_direction = Vector2i(input_direction)
 
 		velocity = input_direction * speed
-		
-		
-		last_direction = input_direction
 		
 		if last_direction.x != 0:  
 			anim.flip_h = last_direction.x > 0
 		
-		if direction_run.has(last_direction):
-			play_animation(direction_run[last_direction])
+		play_animation(anim_list["run"][last_direction])
+
 	else:
 		velocity = Vector2.ZERO  
 
-		
-		if last_direction != Vector2.ZERO and direction_idle.has(last_direction):
-			play_animation(direction_idle[last_direction])
-		else:
-			
-			play_animation("State_Idle_FromSide")
-		
+		play_animation(anim_list["idle"][last_direction])
+
+	move_and_slide()
+
 func play_animation(animation: String) -> void:
 	AnimPlayer.play(animation)
 
@@ -87,5 +87,4 @@ func data() -> Dictionary:
 
 func load_data(_data: Dictionary) -> void:
 	position = Vector2(_data["pos"]["x"], _data["pos"]["y"])
-	last_direction = Vector2(_data["last_dir"]["x"], _data["last_dir"]["y"])
-
+	last_direction = Vector2i(_data["last_dir"]["x"], _data["last_dir"]["y"])
