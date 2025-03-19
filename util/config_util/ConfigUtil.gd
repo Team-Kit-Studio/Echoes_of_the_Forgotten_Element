980ee01b0da9574d7d3ad6744e62c03897c3211c
Mundolor <maksimka0776@gmail.com>
2025-03-19 21:12:31 +0300

    Add files via upload

--- util/config_util/ConfigUtil.gd
+++ util/config_util/ConfigUtil.gd
@@ -19,5 +19,14 @@ static func set_config_dict(data: Dictionary) -> ConfigFile:
 
 	return config
 
+static func set_config_array(data: Array[Dictionary]) -> ConfigFile:
+	var config: ConfigFile = ConfigFile.new()
+	for dict: Dictionary in data:
+		for property: String in dict.keys():
+			config.set_value(dict["section_key"], property, dict[property])
+
+	return config
+		
+
 static func save_config(config: ConfigFile, path: String) -> void:
 	config.save(path)
