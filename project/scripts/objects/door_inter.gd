2b3bd594c1e99f3bdae4be06e899e2e6c75f9266
Mundolor <maksimka0776@gmail.com>
2025-10-02 18:44:19 +0300

    02.10.25
    DoorSpaceShip

--- project/scripts/objects/door_inter.gd
+++ project/scripts/objects/door_inter.gd
@@ -8,8 +8,9 @@ extends RigidBody2D
 @onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
 var in_react = preload("res://project/assets/sprites/Objects/Interactive/Key_cardridder_react.png")
 var out_react = preload("res://project/assets/sprites/Objects/Interactive/Key_cardridder.png")
-var Open:bool = false
+var unlock:bool = false
 var area: bool = false
+var open: bool = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -18,26 +19,28 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		area = true
 		check_item(body)
 		
+		
 func check_item(BodyFunc: Node2D):
 	for key in BodyFunc.DataInventory():
 		if key.item != null and key.item.ItemName == "KeyCardGray":
-			Open = true
+			unlock = true
 			print("Открыто")
 			return
 		else:
-			Open =false
+			unlock =false
 			
 		
 func interactive() -> void:
 	#print("клавиша нажата")
 	var nodes = get_tree().get_nodes_in_group("collision")
-	if Open:
+	if unlock and !open:
 		anim.play("Open")
 		Play_Music("res://project/assets/sounds/MSE/SealedDoorOpen (3).wav")
 		for anima in nodes:
 			var tween: Tween = get_tree().create_tween()
 			tween.tween_property(anima, "position:x", anima.position.x * 3.0625, 0.8)
 		await anim.animation_finished
+		open = !open
 		timer.start(4)
 		await timer.timeout
 		for anima in nodes:
